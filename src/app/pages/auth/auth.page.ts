6e566e5f4461739a16fd13983278fbffd98b11f8
Enrique Díaz Romero <enriqued_93@hotmail.com>
2020-01-06 00:52:50 -0600

    Create Log In and redirect to Auth section when success

--- src/app/pages/auth/auth.page.ts
+++ src/app/pages/auth/auth.page.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthenticationService} from '../../services/authentication.service';
 
 @Component({
-  selector: 'app-auth',
-  templateUrl: './auth.page.html',
-  styleUrls: ['./auth.page.scss'],
+    selector: 'app-auth',
+    templateUrl: './auth.page.html',
+    styleUrls: ['./auth.page.scss'],
 })
 export class AuthPage implements OnInit {
+    userEmail: string;
 
-  constructor() { }
+    constructor(private authenticationService: AuthenticationService) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.userEmail = this.authenticationService.getUserEmail();
+    }
 
 }
--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -5,10 +5,26 @@ import {Injectable} from '@angular/core';
 })
 export class AuthenticationService {
 
+    private user = null;
+
     constructor() {
     }
 
     public isUserAuthenticated(): boolean {
-        return false; // TODO: Change to actual verification
+        return !!this.user; // TODO: Change to actual verification
+    }
+
+    public getUserEmail(): string {
+        return this.user.email;
+    }
+
+    public logIn(email: string, password: string): Promise<boolean> {
+        // TODO: Change to actual login
+        return new Promise((resolve) => {
+            this.user = {
+                email
+            };
+            setTimeout(resolve, 2000, true);
+        });
     }
 }
